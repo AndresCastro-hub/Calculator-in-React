3211d912913b5ca5059667515e3ab4395d9bf37d
Andres Castro <andrescastro2011@hotmail.com>
2022-03-13 18:51:38 -0300

    interfaz calculadora

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
-
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  
+  const [numeroUno, setNumeroOne]= useState('');
+  const [numeroDos, setNumeroDos] = useState('');
+  const [resultado, setResultado] = useState(0); 
+  const [operacionActual, setOperacionActual] = useState('');
+
+  const clickNumber = (valor) => {
+    if( operacionActual === '') {
+      setNumeroOne( numeroUno + valor )
+    }else
+    setNumeroDos( numeroDos + valor)
+  }
+
+  const operation = (valor) => {
+    setOperacionActual(valor)
+  }
+
+  // const resultado = () => {
+  //   setNumeroOne 
+  // }
+
+
   return (
+    <>
+    <h2>Calculadora</h2>
     <div className="App">
-        <h2>Calculadora</h2>
+        <div className='calculadora-grid'>
+          <div className='output'>
+            <div className='operacionAnterior'>{ numeroUno ? numeroUno + operacionActual + numeroDos : ''}</div>
+            <div className='operacionActual'></div>
+          </div>
+          <button onClick={()=> {}} className='span'>AC</button>
+          <button onClick={()=> {}}>DEL</button>
+          <button onClick={() => operation('/')}>/</button>
+          <button onClick={()=> clickNumber(7)}>7</button>
+          <button onClick={()=> clickNumber(8)}>8</button>
+          <button onClick={()=> clickNumber(9)}>9</button>
+          <button onClick={() => operation('*')}>*</button>
+          <button onClick={()=> clickNumber(4)}>4</button>
+          <button onClick={()=> clickNumber(5)}>5</button>
+          <button onClick={()=> clickNumber(6)}>6</button>
+          <button onClick={() => operation('+')}>+</button>
+          <button onClick={()=> clickNumber(1)}>1</button>
+          <button onClick={()=> clickNumber(2)}>2</button>
+          <button onClick={()=> clickNumber(3)}>3</button>
+          <button onClick={() => operation('-')}>-</button>
+          <button onClick={()=>{}}>.</button>
+          <button onClick={()=> clickNumber(0)}>0</button>
+          <button onClick={()=>{}} className='span'>=</button>
+        </div>
     </div>
+    </>
   );
 }
 
